82dd1e4ed8bca0a1a15d276cba59867320838dc8
jjwumaster <jjwumaster@gmail.com>
2017-12-04 15:05:50 -0500

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,7 +1,7 @@
 class SecretsController < ApplicationController
 
   def show
-    redirect_to 'controller: 'sessions', action: 'new'' unless session[:name]
+    redirect_to 'controller: 'sessions', action: 'new'' unless current_user
   end
 
 end
